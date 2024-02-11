80e549cfcf025ba5559757524c910d0ae53f08ec
Jungtaek Kim <jungtaek.kim@pitt.edu>
2024-02-11 16:21:26 -0500

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -17,8 +17,9 @@ author = 'Jungtaek Kim'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
+extensions = [
+    'sphinx.ext.autodoc',
+]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
